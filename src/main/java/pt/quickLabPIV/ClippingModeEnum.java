10f14e5e3539e5200d6b118df65966dadb7f77d7
CoreRasurae <luis.p.mendes@gmail.com>
2023-08-28 22:59:51 +0100

    Update all source files to include SPDX license information

--- src/main/java/pt/quickLabPIV/ClippingModeEnum.java
+++ src/main/java/pt/quickLabPIV/ClippingModeEnum.java
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ * QuickLabPIV-ng - A hybrid PIV and PIV software laboratory (new generation)
+ *
+ * Copyright (C) 2020 Luís Mendes
+ */
 package pt.quickLabPIV;
 
 public enum ClippingModeEnum {
